54eedf7d5d2900e466f669f3d1b54ae6fa2e4c71
R1M19E19 <garcia.209134611.2c@gmail.com>
2018-05-18 15:03:54 +0200

    elnuevo

--- pos/src/app/ramses/ramses.component.ts
+++ pos/src/app/ramses/ramses.component.ts
@@ -8,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class RamsesComponent implements OnInit {
 
   constructor() { }
-
+X="Selfies con el iPhone X";
+liga="Más información";
+tomar="Cómo tomar fotos";
+coloritos="con el iPhone.";
+I="iCloud";
+mejor="El mejor lugar para todas tus fotos, archivos y mucho más.";
+letritas="Todos los dispositivos Apple vienen con iCloud, para que tu contenido siempre esté protegido, actualizado y a la mano dondequiera que estés.";
+nina="Tú quieres lo mejor para tu familia,nosotros también.";
+ilegal="En Apple constantemente estamos desarrollando nuevas funcionalidades para asegurarnos de que tus hijos usen sus dispositivos como tú quieres que lo hagan.";
   ngOnInit() {
   }
 
